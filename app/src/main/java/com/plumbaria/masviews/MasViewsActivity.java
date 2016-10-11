e8336d8150994f29ac0913db300b20b028177615
Quico Orozco <esi@plumbaria.com>
2016-10-11 20:44:04 +0200

    Finalizado 2.9

--- app/src/main/java/com/plumbaria/masviews/MasViewsActivity.java
+++ app/src/main/java/com/plumbaria/masviews/MasViewsActivity.java
@@ -2,6 +2,8 @@ package com.plumbaria.masviews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MasViewsActivity extends AppCompatActivity {
 
@@ -10,4 +12,6 @@ public class MasViewsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mas_views);
     }
-}
+    public void sePulsa(View view){
+        Toast.makeText(this, "Pulsado", Toast.LENGTH_SHORT).show();
+    }}
