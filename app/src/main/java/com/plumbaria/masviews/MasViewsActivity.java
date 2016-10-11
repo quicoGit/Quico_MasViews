bd22c64610e4e1de50a5eaede79203668547c52d
Quico Orozco <esi@plumbaria.com>
2016-10-11 20:19:26 +0200

    Initial commit

---
+++ app/src/main/java/com/plumbaria/masviews/MasViewsActivity.java
@@ -0,0 +1,13 @@
+package com.plumbaria.masviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MasViewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mas_views);
+    }
+}
