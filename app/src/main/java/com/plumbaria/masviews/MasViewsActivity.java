9e6305a8f39dfe9e78c7cfe540c324ebf0c64cdd
Quico Orozco <esi@plumbaria.com>
2016-10-12 10:37:20 +0200

    Finalizado 20.10

--- app/src/main/java/com/plumbaria/masviews/MasViewsActivity.java
+++ app/src/main/java/com/plumbaria/masviews/MasViewsActivity.java
@@ -3,15 +3,26 @@ package com.plumbaria.masviews;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MasViewsActivity extends AppCompatActivity {
 
+    private EditText entrada;
+    private TextView salida;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mas_views);
+        entrada = (EditText)findViewById(R.id.entrada);
+        salida = (TextView)findViewById(R.id.salida);
     }
     public void sePulsa(View view){
-        Toast.makeText(this, "Pulsado", Toast.LENGTH_SHORT).show();
-    }}
+        salida.setText(String.valueOf(
+                Float.parseFloat(entrada.getText().toString())*2.0));
+    }
+    public void sePulsa0(View view){
+        entrada.setText(entrada.getText()+(String)view.getTag());
+    }
+}
